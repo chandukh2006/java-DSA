1d3d34dc8f5d54fe3d8da883a789f018b60e4877
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2024-09-25 19:34:31 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+#Similarly for strings
+public class Main{
+    public static void main(String[] args){
+        String name="chandu";
+        char target='h';
+    
+        System.out.println(Linear(name, target));
+       
+
+    }
+    static boolean Linear(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+        for(int i=0;i<str.length();i++){
+            if(target==str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
