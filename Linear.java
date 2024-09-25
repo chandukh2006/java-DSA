734795a164821daf770b25785448d5b48d9e6d7f
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2024-09-25 19:30:07 +0530

    Add files via upload
    Similarly for strings

--- Linear.java
+++ Linear.java
@@ -1,21 +1,22 @@
 public class Linear{
     public static void main(String[] args){
-        int[] arr={10,20,50,60,55};
-        int target=60;
-        int answer=linearsearch(arr,target);
-        System.out.println(answer);
+        String name="chandu";
+        char target='h';
+    
+        System.out.println(Linear(name, target));
+       
 
     }
-static int linearsearch(int [] arr,int target){
-if(arr.length==0){
-    return 0;
-}
-for(int i=0;i<arr.length;i++){
-    int element=arr[i];
-    if(element==target){
-        return i;
+    static boolean Linear(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+        for(int i=0;i<str.length();i++){
+            if(target==str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
     }
-}
-return -1;
-}
+
 }
