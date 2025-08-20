18ef8936feb385ca00c01b833479f44851d8fda9
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2025-08-20 19:26:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,30 @@
-even or odd
-import java.util.Scanner;
-
-public class Main{
+import java.util.*;
+public class Main {
 
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+        int [] arr = {2,4,6,8,10};
+        int key =8;
+        int ans = binary(arr, key);
 
-        System.out.print("Enter a number: ");
-        int number = input.nextInt();
-
-        if (isEven(number)) {
-            System.out.println(number + " is even.");
-        } else {
-            System.out.println(number + " is odd.");
-        }
+        System.out.println(ans);
+        
     }
+    public static int binary(int arr[], int key){
+        int start = 0;
+        int end =arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(key==arr[mid]){
+                return mid;
+            }
+             if(arr[mid]<key){
+                start=mid+1;
+             }
+             else{
+                end = mid-1;
+             }
+        }
+        return -1;
 
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
     }
 }
