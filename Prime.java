2109eae359799e87ea537cab3459cbdd00a7ba88
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2025-09-12 08:47:29 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -9,6 +9,8 @@ public class prime {
     }
     public static boolean isprime(int n ){
         if(n<=1) return false;
+         if (n % 2 == 0) {
+            return false;
         for(int i = 3;i<=Math.sqrt(n);i+=2){
             if(n%i==0){
                 return false;
