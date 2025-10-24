37cd442a1c9582769dd599db31122f40e2ce0df5
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2025-10-24 08:08:21 +0530

    Update stack_arraylist.java

--- stack_arraylist.java
+++ stack_arraylist.java
@@ -1,39 +1,44 @@
-import java.util.ArrayList;
-public class StackIMP {
-    static class stack{
-        static ArrayList<Integer> list = new ArrayList<>();
+import java.util.*;
+public class Stacki{
+    static class Stack{
+        ArrayList<Integer> list = new ArrayList<>();
 
-        public static boolean isEmpty(){
-            return list.size()==0;
-        }
+    public boolean isEmpty(){
+        return list.size()==0;
+    }
 
-        //push
-        public static void push(int data) {
-            list.add(data);
-        }
+    //push
 
-        //pop
-        public static int pop(){
-            if(isEmpty()) return -1;
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
-            return top;
+    public void push(int data){
+        list.add(data);
+    }
+    ///pop
+    public int  pop(){
+        if(isEmpty()){
+            return -1;
         }
-        //peek
-        public static int peek(){
-            if(isEmpty()) return -1;
-            return list.get(list.size()-1);
+         int top = list.get(list.size()-1);
+         list.remove(list.size()-1);
+         return top;
+    }
+    //peek
+    public int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println("peek element is"+ s.peek());
+            s.pop();
         }
     }
-    public static void main(String[] args){
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
 
-        while(!stack.isEmpty()){
-            System.out.println("the peek elemtn is "+stack.peek());
-            stack.pop();
-        }
-    }
 }
