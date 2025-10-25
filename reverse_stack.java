0ffdf387312d58d4bb3921aa58897cfc92421f7d
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2025-10-25 19:36:36 +0530

    Update reverse_stack.java

--- reverse_stack.java
+++ reverse_stack.java
@@ -51,3 +51,92 @@ class Solution {
 }
 
 
+//custom stack reverse
+import java.util.*;
+
+import org.w3c.dom.Node;
+public class Reverse {
+
+     static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+     static class Stack {
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // Push operation
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // Pop operation
+        public static int pop() {
+            if (isEmpty()) return -1;
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // Peek operation
+        public static int peek() {
+            if (isEmpty()) return -1;
+            return head.data;
+        }
+    }
+
+    public static void insertBottom(Stack s ,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return ;
+        }
+        int top = s.pop();
+        insertBottom(s,data);
+        s.push(top);
+    }
+    public static void reverse(Stack s){
+        if(s.isEmpty()){
+            return ;
+        }
+        int top = s.pop();
+        reverse(s);
+        insertBottom(s,top);
+
+    }
+
+    public static void main(String[] args){
+        Stack s = new Stack();
+        s.push(4);
+        s.push(3);
+        s.push(2);
+        s.push(1);
+        Node temp = s.head;
+
+        while(temp!=null){
+            System.out.println(temp.data +" ");
+            temp = temp.next;
+
+        }
+        reverse(s);
+        temp = s.head;
+       System.out.println("the reversed stack is ");
+        while(temp!=null){
+            System.out.println(temp.data +" ");
+            temp = temp.next;
+
+        }
+        
+    }
+}
+
+
