687312f4f0c5b2ea7f15a02fb11ab742ec578605
Chandu K H <159919951+chandukh2006@users.noreply.github.com>
2025-10-26 20:20:53 +0530

    Update activity_selection(greedy).java

--- activity_selection(greedy).java
+++ activity_selection(greedy).java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {                                                                    //tc O(nlogn) and sc O(n)
     public int activitySelection(int[] start, int[] finish) {
         // code here
         int n = start.length;
@@ -24,3 +24,36 @@ class Solution {
         return count;
     }
 }
+
+
+///approach 2    same tc ans sc as above approach
+import java.util.*;
+
+class Solution {
+    public int activitySelection(int[] start, int[] finish) {
+        int n = start.length;
+
+        // Create a list of activities as pairs [start, finish]
+        ArrayList<int[]> activities = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            activities.add(new int[]{start[i], finish[i]});
+        }
+
+        // Sort activities by finish time
+        activities.sort(Comparator.comparingInt(a -> a[1]));
+
+        // Select activities greedily
+        int count = 1;                // first activity is always selected
+        int lastFinish = activities.get(0)[1];  // finish time of last selected activity
+
+        for (int i = 1; i < n; i++) {
+            if (activities.get(i)[0] >= lastFinish) {  // check start >= last finish
+                count++;
+                lastFinish = activities.get(i)[1];
+            }
+        }
+
+        return count;
+    }
+}
+
